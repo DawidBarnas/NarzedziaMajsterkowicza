75fa9efd48cd074be77c9ee7c977d495078295fb
kapizwa <kac.gorka@gmail.com>
2021-12-06 17:12:35 +0100

    1.4
    Do aplikacji zostało dodane narzędzie zajmujące się pomiarem temperatury.

--- app/src/main/java/com/example/narzdziamajsterkowicza/NachylenieAc.java
+++ app/src/main/java/com/example/narzdziamajsterkowicza/NachylenieAc.java
@@ -2,13 +2,65 @@ package com.example.narzdziamajsterkowicza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class NachylenieAc extends AppCompatActivity {
+import java.text.BreakIterator;
+
+public class NachylenieAc extends AppCompatActivity implements SensorEventListener {
+    private TextView textview;
+    private SensorManager sensorManager;
+    private Sensor tempSensor;
+    private Boolean isTemperatureSensoreAvailable;
+    private BreakIterator textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nachylenie);
+
+        textview = findViewById(R.id.textView3);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)!=null)
+        {
+            tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+            isTemperatureSensoreAvailable = true;
+        }else{
+            textView.setText("Temperature sensor is not Availible");
+            isTemperatureSensoreAvailable = false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+textview.setText(sensorEvent.values[0]+" °C");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isTemperatureSensoreAvailable){
+            sensorManager.registerListener(this, tempSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(isTemperatureSensoreAvailable){
+            sensorManager.unregisterListener(this);
+        }
     }
 }
